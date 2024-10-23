742f7be310a2b700f89ee2059c30510c040c8ebc
AnastasiiaSamborska <samboa9614172_aehit@students.vizja.pl>
2024-10-23 02:13:53 +0200

    Update DivisorsLib.cpp
    update

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -3,6 +3,7 @@
 
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
+	/*
 	std::vector<unsigned long long> result;
 	for (unsigned long long i = 1; i <= number; ++i)
 	{
@@ -10,4 +11,22 @@ std::vector<unsigned long long> divisors(unsigned long long number)
 			result.push_back(i);
 	}
 	return result;
+	*/
+
+    std::vector<unsigned long long> result;
+    unsigned long long squareRoot = sqrt(number); 
+
+    for (unsigned long long i = 1; i <= squareRoot; ++i)
+    {
+        if (number % i == 0)
+        {
+            result.push_back(i); 
+            if (i != number / i)
+            {
+                result.push_back(number / i); 
+            }
+        }
+    }
+
+    return result;
 }
